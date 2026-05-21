e65eb465164a4323acb5dec75c240d31a5065f51
Bruna <bruna.valera.marques@escola.pr.gov.br>
2026-05-21 19:04:45 -0300

    Aula 02- funções callbacks

--- ex02.js
+++ ex02.js
@@ -1,9 +1,9 @@
 function validarSenha(senha) {
-    return senha.length >= 8;
+    return senha.length >= 8
 }
-const senhaUsuario = prompt("Digite a sua senha:");
+const senhaUsuario = prompt("Digite a sua senha:")
 if (validarSenha(senhaUsuario)) {
-    console.log("Senha valida");
+    console.log("Senha valida")
 } else {
-    console.log("Senha fraca - minimo 8 caracteres");
+    console.log("Senha fraca - minimo 8 caracteres")
 }
--- ex03.js
+++ ex03.js
@@ -1,13 +1,13 @@
 
-const somar = (a, b) => a + b;
-const subtrair = (a, b) => a - b;
-const multiplicar = (a, b) => a * b;
-const dividir = (a, b) => b === 0 ? "Erro" : a / b;
+const somar = (a, b) => a + b
+const subtrair = (a, b) => a - b
+const multiplicar = (a, b) => a * b
+const dividir = (a, b) => b === 0 ? "Erro" : a / b
 
-const num1 = parseFloat(prompt("Digite o 1° número:"));
-const num2 = parseFloat(prompt("Digite o 2° número:"));
+const num1 = parseFloat(prompt("Digite o 1° número:"))
+const num2 = parseFloat(prompt("Digite o 2° número:"))
 
-console.log(`Soma: ${somar(num1, num2)}`);
-console.log(`Subtração: ${subtrair(num1, num2)}`);
-console.log(`Multiplicação: ${multiplicar(num1, num2)}`);
-console.log(`Divisão: ${dividir(num1, num2)}`);
+console.log(`Soma: ${somar(num1, num2)}`)
+console.log(`Subtração: ${subtrair(num1, num2)}`)
+console.log(`Multiplicação: ${multiplicar(num1, num2)}`)
+console.log(`Divisão: ${dividir(num1, num2)}`)
