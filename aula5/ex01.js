f6d6805b749270acc638d9f83a66700b5012d6e5
Brug-buo <bruna.valera.marques@escola.pr.gov.br>
2026-06-30 08:44:19 -0300

    aula5

--- aula5/ex01.js
+++ aula5/ex01.js
@@ -3,15 +3,55 @@ const express = require('express')
 const app = express()
 app.use(express.json())
 
-app.listen(3000, () => {
-console.log(" Servidor rodando em http :// localhost :3000 ");
-});
-
-// app.get('/', (req , res) => {
-// res.json({ mensagem: " Meu servidor esta no ar!" })
-//})
-
 //ex1 
-// app.get ('/sobre', (req , res) => {
-// res.json({aluno: "bruna", diciplina: "backandI", ano: 2026})
-// })
+ app.get ('/', (req , res) => {
+    const eune = [
+         res.json({aluno: "bruna", diciplina: "backandI", ano: 2026})
+    ]
+
+ })
+
+//ex2
+app.get ('/p', (req , res) => {
+    const p = [
+{" id ": 1 ,
+" nome ": " Achocolatado Nescau - 550g " ,
+" preço ": 12.99},
+
+{" id ": 2 ,
+" nome ": " Maionese Hellmann's - 800g " ,
+" preço ":  19.90},
+
+{" id ": 3 ,
+" nome ": " Suco de Uva Aurora Tinto Integral - 1,5 Litro " ,
+" nota ": 14.98},
+
+{" id ": 4 ,
+" nome ": "bisnaga" ,
+" nota ": 4.49}
+];
+ res.json(p)
+ })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+app.listen(3000, () => {
+console.log(" Servidor rodando em http://localhost:3000");
+});
--- aula5/ex02.js
+++
@@ -1,7 +0,0 @@
-const express = require('express')
-const app = express()
-app.use(express.json())
-
-app.get('/alunos', (req, res) => {
-    res.json({nome: "Bruna", diciplina: "Desenvolvimento de Sistemas", ano: "2027"})
-})
